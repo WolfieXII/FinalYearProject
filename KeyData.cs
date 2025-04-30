1ff7de032ea030625834d0972e4761e13421b2ea
WolfieXII <81812724+WolfieXII@users.noreply.github.com>
2025-04-30 12:24:46 +0100

    Delete KeyData.cs
    Removing duplicate files.

--- KeyData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-// Logic for setting the type of key for the key objects the agent will be collecting (Red, Green or Blue).
-public enum KeyType { Red, Green, Blue }
-public class KeyData : MonoBehaviour
-{
-    public KeyType keyType;
-}
