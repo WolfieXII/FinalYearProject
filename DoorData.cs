6809d95af01724ad8e0dc2e98364d8528ef01d0d
WolfieXII <81812724+WolfieXII@users.noreply.github.com>
2025-04-30 12:25:14 +0100

    Delete DoorData.cs
    Removing duplicate files now that the Assets folder has been added.

--- DoorData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-// Logic for setting the type of door for the door objects the agent will be opening (Red, Green or Blue).
-public class DoorData : MonoBehaviour 
-{
-    public KeyType doorType;
-}
